85eed7e2e55c84ad5f0b07ac2ef3c60dccaab002
tenaibr <tenaibr@gmx.fr>
2024-06-16 19:28:38 +0200

    .

--- calc.py
+++ calc.py
@@ -4,7 +4,8 @@ class Token:
         self.value = value
 
     def __repr__(self):
-        return f"Token({self.type}, {self.value})"
+        return f"Token({self.type}, {repr(self.value)})"
+
 
 def tokeniser(string: str):
     tokens = []
@@ -14,9 +15,27 @@ def tokeniser(string: str):
                 tokens[-1].value = tokens[-1].value * 10 + int(string[i])
             else:
                 tokens.append(Token("INTEGER", int(string[i])))
+        elif string[i] == " ":
+            pass
         elif string[i] == "+":
             tokens.append(Token("PLUS", "+"))
+        elif string[i] == "*":
+            tokens.append(Token("TIMES", "*"))
+        elif string[i] == "-":
+            tokens.append(Token("MINUS", "-"))
+        elif string[i] == "/":
+            tokens.append(Token("DIVIDE", "/"))
+        elif string[i] == "(":
+            tokens.append(Token("LPAREN", "("))
+        elif string[i] == ")":
+            tokens.append(Token("RPAREN", ")"))
+        else:
+            raise SyntaxError(f"{i} {string[i]}")
     return tokens
+
+
+def step(tokens: list):
+    left = tokens[0]
 
 
 def syntax_check(string: str):
@@ -25,7 +44,7 @@ def syntax_check(string: str):
         if compt < 0:
             return False
         if string[i] == "(":
-            compt +=1
+            compt += 1
         elif string[i] == ")":
             compt -= 1
     for i in range(len(string)-1):
@@ -35,10 +54,13 @@ def syntax_check(string: str):
 
 # print(syntax_check("183)7(3+g)dyd+"))
 
+
 def main(chaine: str):
     if syntax_check(chaine):
         tokens = tokeniser(chaine)
         print(tokens)
 
+
 if __name__ == "__main__":
     main(input("calc>"))
+
